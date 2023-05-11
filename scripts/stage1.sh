5b5c866c71062b825c31b3000a254443c3b24daa
Yusuf Roshdy <yusufroshdy9@gmail.com>
2023-05-11 13:44:18 +0300

    Add scoop to stage 1

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+psql -U postgres -c 'DROP DATABASE IF EXISTS project;'
+
+psql -U postgres -c 'CREATE DATABASE project;'
+
+psql -U postgres -d project -f sql/db.sql
+
+sqoop import-all-tables \
+    --connect jdbc:postgresql://localhost/project \
+    --username postgres \
+    --warehouse-dir /project \
+    --as-avrodatafile
