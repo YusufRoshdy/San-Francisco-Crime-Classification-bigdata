4cd3d5ef10debc3ac72069500ed2125783b59008
Yusuf Roshdy <yusufroshdy9@gmail.com>
2023-05-11 14:44:20 +0300

    Add stage 1 commands

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -6,7 +6,11 @@ psql -U postgres -c 'CREATE DATABASE project;'
 psql -U postgres -d project -f sql/db.sql
 
 sqoop import-all-tables \
+    -Dmapreduce.job.user.classpath.first=true \
     --connect jdbc:postgresql://localhost/project \
     --username postgres \
     --warehouse-dir /project \
-    --as-avrodatafile
+    --as-avrodatafile \
+    --compression-codec=snappy \
+    --outdir /project/avsc \
+    --m 1
