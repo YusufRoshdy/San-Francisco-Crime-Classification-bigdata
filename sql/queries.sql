37cf57a1a6e448e8b44c7063285b65f45ff4cbca
hussein987 <husseinyounes2015@gmail.com>
2023-05-14 12:04:56 +0300

    added sql queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -5,3 +5,6 @@ SELECT * FROM crime_data WHERE category = 'WARRANTS';
 SELECT * FROM crime_data WHERE category = 'WARRANTS' AND day_of_week = 'Monday';
 
 SELECT pd_district, COUNT(*) FROM crime_data GROUP BY pd_district ORDER BY COUNT(*) DESC;
+
+-- find the number of crimes for each week
+SELECT date_trunc('week', dates) AS week, COUNT(*) FROM crime_data GROUP BY week ORDER BY week;
